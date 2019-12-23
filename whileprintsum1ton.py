b23ba084ba0d1b0dc4fef24637b41d524524794d
jeevithac1999 <59138170+jeevithac1999@users.noreply.github.com>
2019-12-23 20:34:45 +0530

    Add files via upload

---
+++ whileprintsum1ton.py
@@ -0,0 +1,6 @@
+i,s=1,0
+n=int(input("enter n"))
+while(i<=n):
+    s+=i
+    i+=1
+print(s)
---
+++ whileprintsumodd_1_to_n.py
@@ -0,0 +1,7 @@
+n=int(input("enter n:"))
+s,i=0,1
+while(i<=n):
+    if i%2!=0:
+        s+=i
+    i+=1
+print(s)
